ee3828444b195bae57ce82a64dcd3fdb8a5e247d
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-08-17 15:17:25 +0200

    add install over http/https

--- src/dojeb.py
+++ src/dojeb.py
@@ -12,6 +12,7 @@ import os
 import shutil
 import os.path
 import zipfile
+import tempfile
 
 
 if __name__ == '__main__':
@@ -21,9 +22,18 @@ if __name__ == '__main__':
 
     if len(sys.argv[1]) < 1:
         print('''RTFM: https://github.com/smok-serwis/pojeb
-Pass a .jeb file to install''')
+Pass a .jeb file to install or an URL with a jebfile to download''')
         sys.exit(1)
 
+    if sys.argv[1].startswith('http'):
+        f_name = tempfile.mktemp()
+        if os.system('wget %s -O %s' % (sys.argv[1], f_name)) != 0:
+            print('wget failed')
+            sys.exit(1)
+        sys.argv[1] = f_name
+    else:
+        f_name = None
+        
     # what is the package named?
     pkname = os.path.splitext(os.path.split(sys.argv[1])[1])[0]
 
@@ -53,5 +63,8 @@ Pass a .jeb file to install''')
     os.chdir(cur_wd)
     shutil.rmtree(temp_d, ignore_errors=True)
 
+    if f_name is not None:
+        os.unlink(f_name)
+    
     # thank you for being an awesome user of pojeb
     sys.exit(rc)
